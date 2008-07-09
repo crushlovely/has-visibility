b34acc4a3dcad6312bad31766daa7254461cc09e
PJ Kelly <pjkelly@Hemera.local>
2008-07-08 20:06:59 -0700

    Hooked up find_visible Singleton method to take a conditions hash just like the plain AR find method.

--- lib/has_visibility.rb
+++ lib/has_visibility.rb
@@ -13,8 +13,9 @@ module Boom
     end
 
     module SingletonMethods
-      def find_visible
-        find(:all, :conditions => {:is_visible => true})
+      def self.find_visible(which = :all, conds = {})
+        (conds[:conditions] ||= {}).merge!({:is_visible => true})
+        find(which, conds)
       end
     end
     
