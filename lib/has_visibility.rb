8b00043d7c6c83174e79b4f55c348e41536e3e7a
PJ Kelly <pjkelly@Hemera.local>
2008-07-08 20:10:09 -0700

    Added bang methods for each of our three instance methods so we can skip the monotony of calling save if we feel like it.

--- lib/has_visibility.rb
+++ lib/has_visibility.rb
@@ -5,7 +5,6 @@ module Boom
     end
     
     module ClassMethods
-      # has_visibility
       def has_visibility
         include Boom::HasVisibility::InstanceMethods
         extend Boom::HasVisibility::SingletonMethods
@@ -20,20 +19,34 @@ module Boom
     end
     
     module InstanceMethods
+
       def toggle_visibility
         self.is_visible = self.is_visible? ? false : true
+      end
+
+      def toggle_visibility!
+        self.toggle_visibility
         self.save
       end
 
       def hide
         self.is_visible = false
+      end
+
+      def hide!
+        self.hide
         self.save
       end
 
       def show
         self.is_visible = true
+      end
+
+      def show
+        self.show
         self.save
       end
+
     end
   end
 end
